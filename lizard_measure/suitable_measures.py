02ee08bb3a2164206571f6587ccfc83f3c6d395b
swinkels <swinkels.pieter@yahoo.com>
2012-02-22 13:43:01 +0100

    Refactors current code to compute the suitable measures
    This commit belongs to lizardsystem/lizard-portal#18.

--- lizard_measure/suitable_measures.py
+++ lizard_measure/suitable_measures.py
@@ -7,13 +7,15 @@
 
 class SuitableMeasures(object):
 
-    def __init__(self, pattern_matcher):
+    def __init__(self, pattern_measures_retriever, pattern_matcher):
+        self.pattern_measures_retriever = pattern_measures_retriever
         self.pattern_matcher = pattern_matcher
 
     def get(self, area):
         """Return the list of suitable measures for the given area."""
         suitable_measures = []
-        for pattern, measures in self.available_patterns.iteritems():
+        measures_map = self.pattern_measures_retriever.retrieve(area.data_set)
+        for pattern, measures in measures_map.items():
             if self.pattern_matcher.matches(area.esf_pattern, pattern):
                 suitable_measures += measures
         return suitable_measures
--- lizard_measure/suitable_measures_tests.py
+++ lizard_measure/suitable_measures_tests.py
@@ -11,30 +11,41 @@ from mock import Mock
 
 from lizard_measure.suitable_measures import SuitableMeasures
 
+
+class SimplePatternMeasuresRetriever(object):
+
+    def __init__(self, measures_map):
+        self.retrieve = Mock(return_value=measures_map)
+
+
 class SimplePatternMatcher(object):
 
     def matches(self, area_pattern, pattern):
         return area_pattern == pattern
 
+
 class SuitableMeasures_get_TestSuite(TestCase):
 
     def test_a(self):
         """Test when there are no ESF patterns."""
-        area = "don't care"
-        suitable_measures = SuitableMeasures(SimplePatternMatcher())
-        suitable_measures.available_patterns = {}
+        area = Mock(esf_pattern='---------', data_set="don't care")
+        suitable_measures = \
+            SuitableMeasures(SimplePatternMeasuresRetriever({}),
+                             SimplePatternMatcher())
         self.assertEqual([], suitable_measures.get(area))
 
     def test_b(self):
         """Test when there is a single ESF pattern that is not suitable."""
-        area = Mock(esf_pattern='---------')
-        suitable_measures = SuitableMeasures(SimplePatternMatcher())
-        suitable_measures.available_patterns = {'XX-------': ['dummy measure']}
+        area = Mock(esf_pattern='---------', data_set="don't care")
+        suitable_measures = \
+            SuitableMeasures(SimplePatternMeasuresRetriever({'XX-------': ['dummy measure']}),
+                             SimplePatternMatcher())
         self.assertEqual([], suitable_measures.get(area))
 
     def test_c(self):
         """Test when there is a single ESF pattern that is suitable."""
-        area = Mock(esf_pattern='XX-------')
-        suitable_measures = SuitableMeasures(SimplePatternMatcher())
-        suitable_measures.available_patterns = {'XX-------': ['dummy measure']}
+        area = Mock(esf_pattern='XX-------', data_set="don't care")
+        suitable_measures = \
+            SuitableMeasures(SimplePatternMeasuresRetriever({'XX-------': ['dummy measure']}),
+                             SimplePatternMatcher())
         self.assertEqual(["dummy measure"], suitable_measures.get(area))
