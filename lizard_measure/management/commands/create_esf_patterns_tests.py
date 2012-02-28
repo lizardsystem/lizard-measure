c242b4fe8831657027bd64889332a2ea1af35694
swinkels <swinkels.pieter@yahoo.com>
2012-02-28 14:57:26 +0100

    Implements new functionality for management command create_esf_patterns
    This commit contains the additional unit tests.
    
    This commit references issue lizardsystem/lizard-portal#18.

--- lizard_measure/management/commands/create_esf_patterns_tests.py
+++ lizard_measure/management/commands/create_esf_patterns_tests.py
@@ -8,10 +8,12 @@
 from django.utils.unittest import TestCase
 
 from lizard_measure.models import EsfPattern
+from lizard_measure.models import KRWWatertype
 from lizard_measure.models import MeasureType
 from lizard_measure.models import WatertypeGroup
 
 from lizard_measure.management.commands.create_esf_patterns import EsfPatterns
+from lizard_measure.management.commands.create_esf_patterns import WatertypeGroups
 
 
 class EsfPatternsTestSuite(TestCase):
@@ -120,3 +122,64 @@ class EsfPatternsTestSuite(TestCase):
         self.assertEqual(2, EsfPattern.objects.count())
 
 
+class WatertypeGroupsTestSuite(TestCase):
+
+    def test_a(self):
+        """Test a single WatertypeGroup is created."""
+        WatertypeGroups().create('M')
+        self.assertEqual('M', WatertypeGroup.objects.get(code='M').code)
+
+    def test_b(self):
+        """Test a watertype references the new WatertypeGroup."""
+        watertype = KRWWatertype()
+        watertype.code = 'M1'
+        watertype.save()
+
+        WatertypeGroups().create('M')
+
+        watertype = KRWWatertype.objects.get(code='M1')
+        self.assertEqual(WatertypeGroup.objects.get(code='M'), watertype.watertype_group)
+
+    def test_c(self):
+        """Test a watertype does not reference the new WatertypeGroup."""
+        watertype = KRWWatertype()
+        watertype.code = 'R1'
+        watertype.save()
+
+        WatertypeGroups().create('M')
+
+        watertype = KRWWatertype.objects.get(code='R1')
+        self.assertEqual(None, watertype.watertype_group)
+
+    def test_d(self):
+        """Test multiple WatertypeGroup(s) are created."""
+        WatertypeGroups().create('M', 'R')
+        self.assertEqual('M', WatertypeGroup.objects.get(code='M').code)
+        self.assertEqual('R', WatertypeGroup.objects.get(code='R').code)
+
+    def test_e(self):
+        """Test a single WatertypeGroup with a two-character code is created."""
+        WatertypeGroups().create('K&O')
+        self.assertEqual('K&O', WatertypeGroup.objects.get(code='K&O').code)
+
+    def test_f(self):
+        """Test a watertype references the new WatertypeGroup."""
+        watertype = KRWWatertype()
+        watertype.code = 'K1'
+        watertype.save()
+
+        WatertypeGroups().create('K&O')
+
+        watertype = KRWWatertype.objects.get(code='K1')
+        self.assertEqual(WatertypeGroup.objects.get(code='K&O'), watertype.watertype_group)
+
+    def test_g(self):
+        """Test a watertype references the new WatertypeGroup."""
+        watertype = KRWWatertype()
+        watertype.code = 'O1'
+        watertype.save()
+
+        WatertypeGroups().create('K&O')
+
+        watertype = KRWWatertype.objects.get(code='O1')
+        self.assertEqual(WatertypeGroup.objects.get(code='K&O'), watertype.watertype_group)
